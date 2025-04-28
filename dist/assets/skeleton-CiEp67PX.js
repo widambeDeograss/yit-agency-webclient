ad3d3f63baa49d7a33b5a3d4c3b6dcdbd9998730
widambedeograss5@gmail.com <dwidambe@mhbbank.co.tz>
2025-04-27 17:10:22 -0700

    feat: update favicon, title, and meta description in index.html

---
+++ dist/assets/skeleton-CiEp67PX.js
@@ -0,0 +1,1 @@
+import{j as s,e as a}from"./index-ux5oNEAr.js";function o({className:e,...t}){return s.jsx("div",{"data-slot":"skeleton",className:a("bg-accent animate-pulse rounded-md",e),...t})}export{o as S};
